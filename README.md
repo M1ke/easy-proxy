010cad41ad38a5d3ea5ea1817b89a1de77f2005b
m1ke <git@m1ke.me>
2017-11-02 11:20:04 +0000

    Update proxy to have authentication via hmac and to pass headers (with optional exclude)

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Create a file `_config.php`:
 <?php
 
 define('BASE', 'https://domain-to-proxy.com/path/');
+define('KEY', 'some-random-key');
 ```
 
 Create a file `_deploy`:
@@ -16,11 +17,39 @@ user@someserver.com:/var/www/path/
 
 Run `./deploy` to upload the files. Now make a request to a URL that hits the server you just uploaded this to.
 
-The script will receive the request, rebuild it using Guzzle and send it on to your target using the same method, body, form data and path.
+The script will receive the request, rebuild it using Guzzle and send it on to your target using the same method, body, form data, path & headers.
 
 The response will be returned to you.
 
-### To do
+### Authentication
 
-* Proxy headers
-* Authentication on proxy (so the proxy can authenticate the original client)
+The request you send to the proxy will require authentication which is provided in the form of an `x-proxy-auth` header which sends a HMAC calculated using a pre-shared key.
+
+For requests without a body you can calculate the HMAC as follows. Let's assume we're making a request to an endpoint `/get/products` on the eventual target of the proxy.
+
+```
+// Note the lack of leading slash in the uri
+$uri = "get/products";
+$x_proxy_auth = hash_hmac('sha256', $uri, KEY);
+```
+
+Assuming we are sending a more complex request, e.g. a POST request with JSON body:
+
+```
+// Note the lack of leading slash in the uri
+$uri = "get/products";
+$data = ['key' => 'value'];
+$request_body = json_encode($data);
+$auth_string = $uri.$request_body;
+$x_proxy_auth = hash_hmac('sha256', $auth_string, KEY);
+```
+
+Obviously any body content should work as long as it is concatenated with the URI as shown. Try and avoid leading or trailing whitespace around the body as it is less certain how this could be interpreted by the proxy when it receives the request.
+
+### Skip forwarding headers
+
+If you wish to not forward on certain headers try adding the following definition to the `_config.php` file. The header names should be lower case and comma separated without spaces.
+
+```
+define('HEADER_EXCLUDE', 'some-header,another-header');
+```
--- index.php
+++ index.php
@@ -27,7 +27,14 @@ if (!defined('BASE')){
 
 	log_to_file($message);
 
-	throw new Exception($message);
+	return new Response('Misconfigured server', Response::HTTP_INTERNAL_SERVER_ERROR);
+}
+if (!defined('KEY')){
+	$message = "You must define a constant called KEY in _config.php to handle request auth";
+
+	log_to_file($message);
+
+	return new Response('Server not configured for auth', Response::HTTP_INTERNAL_SERVER_ERROR);
 }
 
 $request_url = BASE.$uri;
@@ -37,14 +44,51 @@ log_to_file("Request will go to $request_url");
 $client = new Client();
 $options = [];
 
+$auth_uri = $uri[0]==='/' ? substr($uri, 1) : $uri;
+
+$auth_string = $auth_uri;
+
 $body = $inbound_request->getContent();
 if (!empty($body)){
 	$options['body'] = $body;
+	$auth_string .= trim($body);
 }
 
 $form_data = $inbound_request->request->all();
 if (!empty($form_data)){
 	$options['form_params'] = $form_data;
+	$auth_string .= json_encode($form_data);
+}
+
+$headers = $inbound_request->headers->all();
+$xproxyauth = is_array($headers['x-proxy-auth']) ? reset($headers['x-proxy-auth']) : $headers['x-proxy-auth'];
+unset($headers['x-proxy-auth']);
+
+if (empty($xproxyauth)){
+	log_to_file("Request did not sent x-proxy-auth header");
+
+	return new Response('You must send an x-proxy-auth header', Response::HTTP_PROXY_AUTHENTICATION_REQUIRED);
+}
+log_to_file(print_r($headers, true));
+
+$key = KEY;
+$calculated_auth = hash_hmac('sha256', $auth_string, $key);
+if ($calculated_auth!==$xproxyauth){
+	log_to_file("Request auth header was {$xproxyauth} but calculated header was {$calculated_auth}");
+	log_to_file("Auth string used to calculate was '$auth_string'");
+
+	return new Response("The proxy auth header you sent ({$xproxyauth}) did not match the calculated value using the pre-shared key.", Response::HTTP_UNAUTHORIZED);
+}
+
+if (!empty($headers)){
+	if (defined('HEADER_EXCLUDE')){
+		$excludes = explode(',', HEADER_EXCLUDE);
+		foreach ($excludes as $exclude){
+			unset($headers[$exclude]);
+		}
+	}
+
+	$options['headers'] = $headers;
 }
 
 if (!empty($options)){
@@ -52,9 +96,10 @@ if (!empty($options)){
 	log_to_file(print_r($options, true));
 }
 
-log_to_file('Sending request to intended recipient');
+$method = $inbound_request->getMethod();
 
-$returned_response = $client->request($inbound_request->getMethod(), $request_url, $options);
+log_to_file("Sending $method request to intended recipient");
+$returned_response = $client->request($method, $request_url, $options);
 
 log_to_file("Returned response body is: ".$returned_response->getBody());
 
